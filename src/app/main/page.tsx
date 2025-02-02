8538d163e1cb9c5982f35e027ce67dac60bfd3b2
tiaramirch <tiaramirchandani@gmail.com>
2025-02-02 15:58:22 +0800

    add main page background

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,5 +2,18 @@
 import React, { useState } from "react";
 
 export default function Home() {
-  return <div className="bg-none">HOlaaa</div>;
+  return (
+    <>
+      <div className="w-full h-auto bg-black">d</div>
+      <div className="w-full h-10 bg-gradient-to-b from-black to-transparent absolute"></div>
+      <div
+        className="w-full min-h-screen bg-cover bg-top"
+        style={{ backgroundImage: "url('/images/Background.png')" }}
+      >
+        <div className="h-[275vh] flex justify-center items-center text-white text-3xl">
+          Scroll down to see more...
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/ui/topnav.tsx
+++ src/app/ui/topnav.tsx
@@ -4,7 +4,7 @@ import { url } from "inspector";
 
 const TopNav = () => {
   return (
-    <div className="w-svh h-[3.5%] bg-transparent flex justify-center absolute top-10 left-3 right-3 sm:left-16 sm:right-16 border-[3px] border-[#564540] bg-gradient-to-t from-[#453B3B] to-[#675045] rounded-full">
+    <div className="w-svh h-[4%] bg-transparent flex justify-center absolute top-20 left-3 right-3 sm:left-16 sm:right-16 border-[3px] border-[#3d1919] bg-gradient-to-t from-[#3a2312] to-[#b04b2c] rounded-full">
       <div className="w-[80%] flex flex-row justify-between items-center">
         <div>Home</div>
         <div>About</div>
